5e1d2950a77abe540f076eddf936c0c0205040ea
Ameen2000 <Ameen2000@users.noreply.github.com>
2023-06-22 01:16:17 +0100

    Combination Sum

--- backtracking.ml
+++ backtracking.ml
@@ -1,3 +1,11 @@
+let rec nub lst =
+  match lst with
+  | [] as l -> l
+  | [_] as l -> l
+  | h :: t ->
+      if List.mem h t
+      then nub t
+      else h :: (nub t)
 (* find all the subsets of a list *)
 let subsets lst =
   let rec aux lst subset result =
@@ -7,3 +15,17 @@ let subsets lst =
         aux t subset (aux t (h :: subset) result)
   in
   aux lst [] []
+
+(* combination sum *)
+let combination_sum lst target =
+  let rec aux lst cur total result =
+    match lst with
+    | [] -> result
+    | x :: xs ->
+        if (total + x) = target
+        then aux (x :: xs) (x :: cur) (total + x) ((x :: cur) :: result)
+        else if (total + x) < target
+        then aux xs cur total (aux (x :: xs) (x :: cur) (total + x) result)
+        else aux xs cur total result
+  in
+  aux lst [] 0 []
